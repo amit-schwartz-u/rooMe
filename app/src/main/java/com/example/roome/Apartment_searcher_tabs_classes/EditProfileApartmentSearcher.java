ceb3d54ad69872c76a35305ba626e9a0e91b9355
forer11 <lior.forer@cs.huji.ac.il>
2020-07-06 01:44:55 +0300

    Removed the unnecessary button see issue 193

--- app/src/main/java/com/example/roome/Apartment_searcher_tabs_classes/EditProfileApartmentSearcher.java
+++ app/src/main/java/com/example/roome/Apartment_searcher_tabs_classes/EditProfileApartmentSearcher.java
@@ -177,14 +177,6 @@ public class EditProfileApartmentSearcher extends Fragment {
                 }
             }
         });
-        Button deleteUserAccount =
-                getView().findViewById(R.id.btn_delete_as_user);
-        deleteUserAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showSignOutDialog();
-            }
-        });
         addRedStarToTextView(R.id.tv_age,"Age");
         addRedStarToTextView(R.id.tv_phoneNumber,"Phone number");
         validateUserInput();
